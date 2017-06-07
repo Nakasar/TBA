c40c69a727332cb94f8e52c8a0e7076780e36191
THIZY Kevin <kevin.thizy@outlook.fr>
2017-06-07 09:50:15 +0200

    added Light Slider control

--- app/src/main/java/tech/orochi/testtba/LightSlider.java
+++ app/src/main/java/tech/orochi/testtba/LightSlider.java
@@ -5,14 +5,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
- * Created by Utilisateur on 07/06/2017.
+ * Created by Kevin on 07/06/2017.
  */
 
 public class LightSlider extends Fragment {
+    TextView lightVolumeText;
+    SeekBar lightVolume;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        lightVolumeText = (TextView) getView().findViewById(R.id.lightVolumeText);
+        lightVolume = (SeekBar) getView().findViewById(R.id.lightVolume);
+        lightVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                lightVolumeText.setText("p - " + seekBar.getId() + " : " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         return inflater.inflate(R.layout.light_slider, container, false);
     }
 }
